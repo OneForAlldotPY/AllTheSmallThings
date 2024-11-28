d48f5563e5bc63d7f93983cca4346f528687bfb5
OneForAll <vllds@pm.me>
2024-11-28 16:57:19 +0100

    Something useful

---
+++ Hyatt Corp.mm
@@ -0,0 +1,71 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1727206399264" ID="ID_1300745551" MODIFIED="1727300679462" TEXT="Hyatt Corp">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Notes of what not to look for: Unsupported browsers, broken link hijacking, tabnabbing, content spoofing, text injection, physical access attacks, self-XSS, self-DoS, clickjacking on non-sensitive actions, CSRF on non-sensitive forms, permissive CORS, software version disclosure, descriptive error messages, CSV injection, open redirects without impact, SSL/TLS issues, lack of SSL pinning, missing jailbreak detection, cookie handling issues, optional security features, rate limiting issues, DoS, DDoS, social engineering attacks, noisy attacks, physical facility attacks.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Notes of what to look for: SQL Injection, Command Injection, XML Injection, Broken Authentication, Credential Stuffing, Insecure Password Recovery, Insecure Direct Object References (IDOR), Privilege Escalation, Insecure Storage of Sensitive Data, Insufficient Transport Layer Protection, Stored XSS, Reflected XSS, DOM-based XSS, Remote Code Execution (RCE), Server-Side Request Forgery (SSRF), Business Logic Errors, Denial of Service (DoS) (if explicitly allowed), Misconfigurations, Third-Party Component Vulnerabilities.
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1727300752273" ID="ID_751006783" MODIFIED="1727300809880" POSITION="left" TEXT="CIDR">
+<node CREATED="1727300799579" ID="ID_871954631" MODIFIED="1727300801891" TEXT="140.95.0.0/16"/>
+<node CREATED="1727300821171" ID="ID_384266316" MODIFIED="1727300833232" TEXT="213.139.133.32/28"/>
+<node CREATED="1727300839516" ID="ID_1645837244" MODIFIED="1727300846978" TEXT="199.66.248.0/22"/>
+</node>
+<node CREATED="1727368181009" ID="ID_1614562967" MODIFIED="1727368185552" POSITION="right" TEXT="Domains">
+<node CREATED="1727300701370" ID="ID_931753032" MODIFIED="1728425483257" TEXT="www.funjet.com">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Notes of what not to look for: Unsupported browsers, broken link hijacking, tabnabbing, content spoofing, text injection, physical access attacks, self-XSS, self-DoS, clickjacking on non-sensitive actions, CSRF on non-sensitive forms, permissive CORS, software version disclosure, descriptive error messages, CSV injection, open redirects without impact, SSL/TLS issues, lack of SSL pinning, missing jailbreak detection, cookie handling issues, optional security features, rate limiting issues, DoS, DDoS, social engineering attacks, noisy attacks, physical facility attacks.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Notes of what to look for: SQL Injection, Command Injection, XML Injection, Broken Authentication, Credential Stuffing, Insecure Password Recovery, Insecure Direct Object References (IDOR), Privilege Escalation, Insecure Storage of Sensitive Data, Insufficient Transport Layer Protection, Stored XSS, Reflected XSS, DOM-based XSS, Remote Code Execution (RCE), Server-Side Request Forgery (SSRF), Business Logic Errors, Denial of Service (DoS) (if explicitly allowed), Misconfigurations, Third-Party Component Vulnerabilities.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1727341188966" ID="ID_1779069876" MODIFIED="1727341229976" TEXT="Port open = 80 &amp; 443"/>
+<node CREATED="1727341236630" ID="ID_1020455652" MODIFIED="1727341466661" TEXT="Nikto results">
+<node CREATED="1727341494101" ID="ID_1890897643" MODIFIED="1727341501888" TEXT="No CSRF found"/>
+<node CREATED="1727341506380" ID="ID_1529660053" MODIFIED="1727341509901" TEXT="No XSS found"/>
+<node CREATED="1727341515521" ID="ID_1159495616" MODIFIED="1727341533349" TEXT="403 Status in beta and www.funjet.com"/>
+<node CREATED="1727341601773" ID="ID_1553307201" MODIFIED="1727341616928" TEXT="No HTTP HSTS in place"/>
+<node CREATED="1727342217564" ID="ID_261502759" MODIFIED="1727342223218" TEXT="Chaching is disabled"/>
+</node>
+<node CREATED="1727345378238" ID="ID_1771414682" MODIFIED="1727553864412" TEXT="GoBuster (Endpoints)">
+<node CREATED="1727345407671" ID="ID_26957724" MODIFIED="1727345421048" TEXT="/2147"/>
+<node CREATED="1727345427387" ID="ID_303350229" MODIFIED="1727345463681" TEXT="/2279"/>
+<node CREATED="1727345517595" ID="ID_661967088" MODIFIED="1727345549353" TEXT="/3313"/>
+<node CREATED="1727345676390" ID="ID_156839774" MODIFIED="1727345685751" TEXT="/7821"/>
+<node CREATED="1727345691451" ID="ID_870458918" MODIFIED="1727345703495" TEXT="/8601"/>
+<node CREATED="1727345466881" ID="ID_424407323" MODIFIED="1727345515754" TEXT="/advisory_132006"/>
+<node CREATED="1727345520261" ID="ID_1073373966" MODIFIED="1727345662365" TEXT="/edinburgh"/>
+<node CREATED="1727345522368" ID="ID_524309601" MODIFIED="1727345670239" TEXT="/pgc"/>
+<node CREATED="1727345706349" ID="ID_882759872" MODIFIED="1727346046723" TEXT="/sca"/>
+<node CREATED="1727346048637" ID="ID_993402492" MODIFIED="1727346069039" TEXT="/microsoft_vista"/>
+<node CREATED="1727346092544" ID="ID_1703991035" MODIFIED="1727346127728" TEXT="/Identity_Theft"/>
+<node CREATED="1727346104147" ID="ID_1722457914" MODIFIED="1727346123048" TEXT="/add_on"/>
+<node CREATED="1727359178781" ID="ID_1826521659" MODIFIED="1727368279237" TEXT="/55027 &#xa;/LinkClick &#xa;/linksbutton &#xa;/toolsbutton &#xa;/hackergurus &#xa;/glitch &#xa;/17875 &#xa;/rootshell &#xa;/6577 &#xa;/icon_unselect &#xa;/forindex &#xa;/advknowledge &#xa;/ebvg &#xa;/els &#xa;/11449 &#xa;/17865 &#xa;/cold-fusion &#xa;/Dreamweaver &#xa;/LookingGlass &#xa;/phplive &#xa;/amateurgirlsnake &#xa;/10905 &#xa;/helpmain &#xa;/0171 &#xa;/lekohit &#xa;/18934 &#xa;/i30 &#xa;/21673 &#xa;/gbl &#xa;/30661 &#xa;/13651 &#xa;/cowz &#xa;/ducttape &#xa;/140706 &#xa;/hsg &#xa;/left_grey &#xa;/B00005JKHX &#xa;/tragwwg &#xa;/Fort_Wayne &#xa;/displayRegisterForm &#xa;/82415 &#xa;/77690 &#xa;/ControlStructures &#xa;/Jack%20Johnson%20-%20%20Brushfire%20Fairytales &#xa;/88084 &#xa;/48422 &#xa;/72897 &#xa;/MPEG-2 &#xa;/127795 &#xa;/102531 &#xa;/102491 &#xa;/threatmodeling &#xa;/securing_browser &#xa;/005717 &#xa;/135199 &#xa;/135844 &#xa;/kristian &#xa;/Exchange2000 &#xa;/209453 /&#xa;/209453&#xa;/190731&#xa;/GPRS&#xa;/fundamentals-incident&#xa;/snort-acid_solaris9&#xa;/lockyer_brief&#xa;/ShCartIco&#xa;/neon2&#xa;/Buzzer&#xa;/filin&#xa;/alchemists&#xa;/overrun&#xa;/16126924&#xa;/kls&#xa;/galleryLogo_sm&#xa;/infomatikjahr&#xa;/maureendowd&#xa;/t4517&#xa;/t9342&#xa;/t10901&#xa;/produce&#xa;/t14868&#xa;/t14861"/>
+</node>
+</node>
+</node>
+</node>
+</map>
